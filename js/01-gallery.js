6c4a663cc4ab329e2be95ce92338c74b46122ddc
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-05-03 21:52:06 +0300

    jvgkg

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,8 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-
-
 
 for (let i = 0; i < galleryItems.length; i++) {
     const element = galleryItems[i];
@@ -11,37 +10,44 @@ for (let i = 0; i < galleryItems.length; i++) {
 
     const {preview, original, description} = element;
     const item = document.createElement("li");
+    const link = document.createElement("a");
     const image = document.createElement("img");
 
 
     image.src = preview;
     image.alt = description;
+    link.href = original;
 
     image.classList.add("gallery__image");
     image.setAttribute('id', `${index}`);
+    link.classList.add("gallery__link");
 
+    link.appendChild(image);
     item.appendChild(image);
 
 
     gallery.appendChild(item);
 }
 
-const modal = document.querySelector("[data-modal]");
-gallery.addEventListener("click", (event) => {
-    modal.classList.toggle("is-hidden");
 
+const galleryLink = document.querySelectorAll(".gallery__link");
+galleryLink.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+});
+
+
+
+gallery.addEventListener('click', (event) => {
     const clickedImage = event.target;
+    
     const id = clickedImage.id;
     const url = galleryItems[id].original;
     const descr = galleryItems[id].description;
 
-    const image = document.getElementById("modal-image");
-    image.src = url;
-    image.alt = descr;
-   
-    console.log(image);
-})
+    console.log(url);
+    const instance = basicLightbox.create(`<img src="${url} alt = ${descr}>`)
 
-modal.addEventListener("click", (event) => {
-    modal.classList.toggle("is-hidden");
+    instance.show()
 })
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,42 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.es";
+
 // Change code below this line
 
 
 
-let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function () {
-	// do something…
-    console.log(gallery);
+
+const gallery = document.querySelector(".gallery");
+
+
+for (let i = 0; i < galleryItems.length; i++) {
+    const element = galleryItems[i];
+    const index = i;
+
+    const {preview, original, description} = element;
+    const item = document.createElement("li");
+    const link = document.createElement("a");
+    const image = document.createElement("img");
+
+
+    image.src = preview;
+    image.alt = description;
+    link.href = original;
+
+    image.classList.add("gallery__image");
+    link.classList.add("gallery__link");
+    
+    link.appendChild(image);
+    item.appendChild(link);
+
+
+    gallery.appendChild(item);
+}
+
+const galleryLink = document.querySelectorAll(".gallery__link");
+galleryLink.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
 });
 
 
-
-
-
-// const gallery = document.querySelector(".gallery");
-
-
-// for (let i = 0; i < galleryItems.length; i++) {
-//     const element = galleryItems[i];
-//     const index = i;
-
-//     const {preview, original, description} = element;
-//     const item = document.createElement("li");
-//     const link = document.createElement("a");
-//     const image = document.createElement("img");
-
-
-//     image.src = preview;
-//     image.alt = description;
-//     link.href = original;
-
-//     image.classList.add("gallery__image");
-//     link.classList.add("gallery__link");
-    
-//     link.appendChild(image);
-//     item.appendChild(link);
-
-
-//     gallery.appendChild(item);
-// }
+//simplelightbox
+var lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  
