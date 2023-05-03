06169d31941947d9de58a765e34c45adf76639fa
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-05-03 21:12:04 +0300

    jyhvgjyh

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.es";
 // Change code below this line
 
-console.log(galleryItems);
+
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+    console.log(gallery);
+});
+
+
+
+
+
+// const gallery = document.querySelector(".gallery");
+
+
+// for (let i = 0; i < galleryItems.length; i++) {
+//     const element = galleryItems[i];
+//     const index = i;
+
+//     const {preview, original, description} = element;
+//     const item = document.createElement("li");
+//     const link = document.createElement("a");
+//     const image = document.createElement("img");
+
+
+//     image.src = preview;
+//     image.alt = description;
+//     link.href = original;
+
+//     image.classList.add("gallery__image");
+//     link.classList.add("gallery__link");
+    
+//     link.appendChild(image);
+//     item.appendChild(link);
+
+
+//     gallery.appendChild(item);
+// }
